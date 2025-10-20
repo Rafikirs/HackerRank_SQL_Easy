d4a255d4d84c9dd88e4cbf429b4b43e729f207c0
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-20 18:07:02 +0200

    Add select query for cities in the USA
    Add SQL query to select names from cities with a population greater than 120,000 in the USA.

---
+++ select_2.sql
@@ -0,0 +1,11 @@
+# Exercise: Revising the Select Query II
+# URL: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+# Description: A simple query
+
+SELECT NAME
+FROM CITY
+WHERE 1=1
+    AND POPULATION > 120000
+    AND CountryCode = 'USA'
+;
+
