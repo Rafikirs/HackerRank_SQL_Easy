4ee5d1eb1d3dea98345576e89f57594172163e41
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-20 18:17:36 +0200

    Add japanese_cities.sql

---
+++ japanese_cities.sql
@@ -0,0 +1,8 @@
+# Exercise: Japanese Cities' Attributes
+# URL: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+# Description: A very simple query
+
+SELECT *
+FROM CITY
+WHERE CountryCode = 'JPN'
+;
