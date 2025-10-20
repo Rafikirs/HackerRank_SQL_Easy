0f39876f109c95da9ea20a8ed5fd73df9d572c41
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-20 18:12:37 +0200

    Create select_by_id.sql

---
+++ select_by_id.sql
@@ -0,0 +1,9 @@
+# Exercise: Select By ID
+# URL: https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+# Description: A very simple query
+
+SELECT *
+FROM CITY
+WHERE ID = 1661
+;
+
