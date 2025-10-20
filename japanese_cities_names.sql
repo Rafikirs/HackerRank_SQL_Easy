30cd9bb91fe5ee406995a578cd8e888a2683004a
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-20 20:59:51 +0200

    Add japanese_cities_names.sql

---
+++ japanese_cities_names.sql
@@ -0,0 +1,8 @@
+# Exercise: Japanese Cities' Names
+# URL: https://www.hackerrank.com/challenges/japanese-cities-name/problem?isFullScreen=true
+# Description: A very simple query
+
+SELECT NAME
+FROM CITY
+WHERE CountryCode = 'JPN'
+;
