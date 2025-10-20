44b3e6240707d1b3f16a2d97ca4db24858d0f326
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-20 18:18:38 +0200

    Add query for city and state in weather station
    Add a simple query to select city and state from the station.

---
+++ weather_observation_station_1.sql
@@ -0,0 +1,6 @@
+# Exercise: Weather Observation Station 1
+# URL: https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+# Description: A very simple query
+
+SELECT CITY, STATE
+FROM STATION
