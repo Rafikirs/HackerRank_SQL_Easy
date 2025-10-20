14ff3f25b458e7e95756c119ab6487ec7c735a05
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-20 17:58:02 +0200

    Revise description in select_1.py

---
+++ select_1.py
@@ -0,0 +1,9 @@
+# Exercise: Revising the Select Query I
+# URL: https://github.com/Rafikirs/HackerRank_Python_Easy/blob/main/alphabet_rangoli.py
+# Description: A very simple query
+
+SELECT *
+FROM CITY
+WHERE 1=1
+    AND CountryCode = 'USA'
+    AND Population > 100000
