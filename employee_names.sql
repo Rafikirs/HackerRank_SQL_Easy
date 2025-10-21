a77be992c3032834fd3e39c6553d5473e8eab548
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-21 15:38:13 +0200

    Update SQL query to order employee names

---
+++ employee_names.sql
@@ -0,0 +1,8 @@
+# Exercise: Employee Names
+# URL: https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+# Description: Query employee names ordered by name
+
+SELECT name
+FROM Employee
+ORDER BY name
+;
