2e5297aab7cad88a41c785d06cb4d0e62c79fee8
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-22 19:46:30 +0200

    Create revising_average.sql

---
+++ revising_average.sql
@@ -0,0 +1,9 @@
+# Exercise: Revising Aggregations - Averages
+# URL: https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?isFullScreen=true
+# Description: A very simple query
+
+SELECT 
+    AVG(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California'
+;
