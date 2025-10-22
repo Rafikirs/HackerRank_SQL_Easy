1b12975ddfcda6d311eebb7fba95f56594dc5b9b
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-22 19:47:56 +0200

    Create average_population.sql

---
+++ average_population.sql
@@ -0,0 +1,7 @@
+# Exercise: Average Population
+# URL: https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true
+# Description: A very simple query
+
+SELECT ROUND(AVG(POPULATION))
+FROM CITY
+;
