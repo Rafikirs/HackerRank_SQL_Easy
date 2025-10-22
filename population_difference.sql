ae3eac7f5205c69defb2827cbcf26acda3e783bf
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-22 19:56:37 +0200

    Create population_difference.sql

---
+++ population_difference.sql
@@ -0,0 +1,6 @@
+# Exercise: Population Density Difference
+# URL: https://www.hackerrank.com/challenges/population-density-difference/problem?isFullScreen=true
+# Description: A very simple query
+
+SELECT MAX(POPULATION) - MIN(POPULATION)
+FROM CITY
